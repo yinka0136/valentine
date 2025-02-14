37252d44e7f939bfb207bbf41ad5370d2a736ede
yinka0136 <yinka0136@gmail.com>
2025-02-14 01:33:25 +0100

    first commit

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💝",
+    pageTitle: "My baby 💝",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
